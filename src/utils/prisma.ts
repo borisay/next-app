d6eee8b59792a2bf281481373016d7d7b2edfb77
boris.ayupov <boris.ayupov@gmail.com>
2026-03-04 11:31:10 -0500

    return back

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -13,6 +13,12 @@ const prisma =
   });
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
+// //************* from troubleshooting */
+// import { PrismaClient } from "../prisma/generated/client";
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 //****************** */
 // import { PrismaClient } from "../generated/prisma/client";
 // import { withAccelerate } from "@prisma/extension-accelerate";
