48ac954c75dc8f7edb861005cc229a89086fbb6e
boris.ayupov <boris.ayupov@gmail.com>
2026-03-11 10:10:44 -0400

    prisma.ts update

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,14 +1,17 @@
-import { PrismaClient } from "../generated/prisma/edge";
+import { PrismaClient } from "../generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// const prisma =
-//   globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({ accelerateUrl: process.env.DATABASE_URL }).$extends(
+    withAccelerate(),
+  );
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate());
+// const prisma = new PrismaClient({
+//   accelerateUrl: process.env.DATABASE_URL,
+// }).$extends(withAccelerate());
 
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
