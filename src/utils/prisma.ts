2e9400de712029a60d8e5db132d807ce620b7b45
boris.ayupov <boris.ayupov@gmail.com>
2026-03-11 10:19:21 -0400

    prisma.ts update

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -9,9 +9,5 @@ const prisma =
     withAccelerate(),
   );
 
-// const prisma = new PrismaClient({
-//   accelerateUrl: process.env.DATABASE_URL,
-// }).$extends(withAccelerate());
-
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 export default prisma;
