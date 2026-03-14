e0013115b71e5cf8f7ec0fb2d2dc7acca55926c3
boris.ayupov <boris.ayupov@gmail.com>
2026-03-13 22:33:45 -0400

    styling change

--- src/app/(protected)/recipes/[id]/page.tsx
+++ src/app/(protected)/recipes/[id]/page.tsx
@@ -25,7 +25,7 @@ const EditRecipePage = () => {
   }
   if (recipe) {
     return (
-      <div className="container mx-auto p-4">
+      <div className="mx-auto p-4">
         <h1 className="text-3xl font-bold mb-4">
           "Edit recipe: {recipe.name}"
         </h1>
